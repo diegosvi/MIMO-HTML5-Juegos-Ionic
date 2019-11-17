ba0e39ee60ecdf56d865726788c8d45968f3964b
Sergio <sergioat7@gmail.com>
2019-11-17 21:01:34 +0100

    Checked result of the board

--- gamesApp/src/app/pages/tres-en-raya/tres-en-raya.page.ts
+++ gamesApp/src/app/pages/tres-en-raya/tres-en-raya.page.ts
@@ -35,7 +35,51 @@ export class TresEnRayaPage implements OnInit {
           return r;
         }
       });
-      this.turn = this.turn === X ? O : X;
+      this.checkBoard(this.matrix, this.turn, row, column);
+      if (this.gameFinished == false) {
+        this.turn = this.turn === X ? O : X;
+        this.title = "Turno de " + this.turn;
+      }
+    }
+  }
+
+  checkBoard(matrix, currentTurn, posX, posY) {
+    if (!( matrix.map(row => { return row.includes(EMPTY_CELL) })).includes(true)) {
+      this.title = "Empate";
+      this.gameFinished = true;
+    } else {
+      this.gameFinished = this.checkCells(matrix, currentTurn, posX, posY);
+    }
+  }
+
+  checkCells(matrix, value, x, y) {
+    var n = this.dimension;
+    var col = 0;
+    var row = 0;
+    var diag = 0;
+    var rdiag = 0;
+
+    for (var i = 0; i < n; i++) {
+      if (matrix[x][i] === value) row++;
+      if (matrix[i][y] === value) col++;
+      if (matrix[i][i] === value) diag++;
+      if (matrix[i][n - (i + 1)] === value) rdiag++;
+    }
+
+    if (row === n) {
+      // this.setVictoryCells(x*n,1);
+      return true;
+    } else if (col === n) {
+      // this.setVictoryCells(y,n);
+      return true;
+    } else if (diag === n) {
+      // this.setVictoryCells(0,n+1);
+      return true;
+    } else if (rdiag === n) {
+      // this.setVictoryCells(n-1,n-1);
+      return true;
+    } else {
+      return false;
     }
   }
 
