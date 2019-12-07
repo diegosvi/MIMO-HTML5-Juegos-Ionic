23da72c29d11f961642ef04bdecbdde4f7edc76a
Sergio Aragonés <sergioat7@gmail.com>
2019-12-07 18:57:54 +0100

    Added button to clean Ionic Storage. Shown alert message before remove storage.

--- gamesApp/src/app/pages/ranking-list/ranking-list.page.ts
+++ gamesApp/src/app/pages/ranking-list/ranking-list.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { StorageService } from 'src/app/servicios/storage/storage.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-ranking-list',
@@ -10,12 +11,42 @@ export class RankingListPage implements OnInit {
 
   ranking;
 
-  constructor(private storageService: StorageService) {
+  constructor(private storageService: StorageService, private alertController: AlertController) {
     this.storageService.getRanking().then(rankingJSON => {
       this.ranking = JSON.parse(rankingJSON);
     });
   }
 
   ngOnInit() { }
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: '¿Seguro que quieres borrar los datos?',
+      message: 'Se perderá todo el progreso hasta ahora.',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: null
+        }, {
+          text: 'Aceptar',
+          handler: () => {
+            this.cleanStorage();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  cleanStorage() {
+    this.storageService.cleanData().then(_ => {
+      this.storageService.getRanking().then(rankingJSON => {
+        this.ranking = JSON.parse(rankingJSON);
+      });
+    });
+  }
 
 }
